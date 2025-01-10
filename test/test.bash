fa802df574105868083e2e57c821e54f500d707b
makino315 <s23c1131km@s.chibakoudai.jp>
2025-01-10 22:50:47 +0900

    test6

--- test/test.bash
+++ test/test.bash
@@ -6,8 +6,9 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+source install/setup.bash && source install/local_setup.bash
 
-timeout 5 ros2 launch mypkg talk_listen.launch.py &> /tmp/mypkg.log
+timeout 10 ros2 launch mypkg talk_listen.launch.py &> /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep '東京都 東京 の天気は'
